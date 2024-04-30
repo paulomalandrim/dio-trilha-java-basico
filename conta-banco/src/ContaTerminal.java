175857269bb93937ca9368df90eb813513bc18e4
paulomalandrim <paulo.malandrim@gmail.com>
2024-04-29 21:28:08 -0300

    1

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,42 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: conhecer e importar a classe Scanner
+
+        Scanner scanner = new Scanner(System.in);
+        
+        // Exibir as mensagens para o nosso usuário
+
+        System.out.print("Por favor, digite o numero da conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o numero da Agencia: ");
+        String agencia = scanner.nextLine();
+      //  scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String cliente = scanner.nextLine();
+      //  scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        Double saldo = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("***********************************************************************");
+        System.out.println("Ola " + cliente + ", obrigado por criar uma conta em nosso banco, sua agencia e "
+                         + agencia + ", conta " + conta + " e seu saldo " + saldo + " ja esta disponivel para saque");
+        System.out.println("***********************************************************************");
+
+        scanner.close();
+
+        // Obeter pela scanner os valores digitados no terminal
+
+        // exibir a mensagem conta criada
+
     }
 }
